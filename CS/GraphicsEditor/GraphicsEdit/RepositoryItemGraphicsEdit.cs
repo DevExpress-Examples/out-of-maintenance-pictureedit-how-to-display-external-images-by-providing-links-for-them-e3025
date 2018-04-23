d932962ec6481357fb3748693033f41d6214ad55
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/GraphicsEditor/GraphicsEdit/RepositoryItemGraphicsEdit.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraEditors.Registrator;
+using DevExpress.XtraEditors.ViewInfo;
+using DevExpress.XtraEditors.Drawing;
+
+namespace GraphicsEditor
+{
+    //The attribute that points to the registration method 
+    [UserRepositoryItem("RegisterGraphicsEditor")]
+    class RepositoryItemGraphicsEdit : RepositoryItemPictureEdit
+    {
+        // Static constructor should call registration method
+        static RepositoryItemGraphicsEdit() { RegisterGraphicsEditor(); }
+
+        public const string GraphicsEditorName = "GraphicsEdit";
+        public override string EditorTypeName { get { return GraphicsEditorName; } }
+
+        public static void RegisterGraphicsEditor()
+        {
+            EditorRegistrationInfo.Default.Editors.Add(new EditorClassInfo(
+                GraphicsEditorName, typeof(GraphicsEdit), typeof(RepositoryItemGraphicsEdit),
+                typeof(GraphicsEditViewInfo), new PictureEditPainter(), true));
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# PictureEdit - How to display external images by providing links for them
+
+
+<p>The current example illustrates how to customize the default PictureEdit editor so that it can accept a path to a graphic file as an EditValue and display a corresponding image.</p>
+
+<br/>
+
+
